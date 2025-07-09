c7727907b63ee7463f85955fd2cd6c332b4b5141
Juan Manuel Gonzalez <juanmaglz617@gmail.com>
2025-07-09 12:44:02 -0300

    Primer commit proyecto TuCancha

---
+++ src/main/java/com/jupilu/TuCancha/TuCanchaApplication.java
@@ -0,0 +1,13 @@
+package com.jupilu.TuCancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuCanchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuCanchaApplication.class, args);
+	}
+
+}
